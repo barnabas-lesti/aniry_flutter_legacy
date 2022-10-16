e850767e602b1056239d34c57ef7252a34f75099
Barnabas Lesti <barnabas.lesti@gmail.com>
2022-10-16 16:09:08 +0200

    Removed app widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
-import 'package:aniry_shopping_list/app/app.dart';
+import 'package:aniry_shopping_list/shopping/screen.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const App());
+void main() => runApp(
+      MaterialApp(
+        title: 'Aniry Shopping List',
+        theme: ThemeData(primarySwatch: Colors.indigo),
+        home: const ShoppingScreen(),
+      ),
+    );
