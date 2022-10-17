e19f25f0506c0e80ccbd38b11af00eb3a2921128
Barnabas Lesti <barnabas.lesti@gmail.com>
2022-10-17 04:09:26 +0200

    Moved realm config to service.

--- lib/app/realm.dart
+++
@@ -1,6 +0,0 @@
-import 'package:aniry_shopping_list/shopping/item.dart';
-import 'package:realm/realm.dart';
-
-final appRealm = Realm(Configuration.local([
-  ShoppingItem.schema,
-]));
--- lib/shopping/service.dart
+++ lib/shopping/service.dart
@@ -1,26 +1,31 @@
-import 'package:aniry_shopping_list/app/realm.dart';
 import 'package:aniry_shopping_list/shopping/item.dart';
 import 'package:realm/realm.dart';
 
 typedef ShoppingServiceResult<T extends RealmObject> = RealmResults<T>;
 
 class _ShoppingService {
+  late final Realm _realm;
+
+  _ShoppingService() {
+    _realm = Realm(Configuration.local([ShoppingItem.schema]));
+  }
+
   ShoppingItem createItem(String text, int order) => ShoppingItem(Uuid.v4(), text, order);
 
-  ShoppingServiceResult<ShoppingItem> getAllItems() => appRealm.all<ShoppingItem>();
+  ShoppingServiceResult<ShoppingItem> getAllItems() => _realm.all<ShoppingItem>();
 
   List<ShoppingItem> sortItemsByOrder(List<ShoppingItem> items) => items..sort((a, b) => a.order.compareTo(b.order));
 
-  void addItem(String text, int order) => appRealm.write(() => appRealm.add(createItem(text, order)));
+  void addItem(String text, int order) => _realm.write(() => _realm.add(createItem(text, order)));
 
-  void checkItem(ShoppingItem item, bool checked) => appRealm.write(() => item.checked = checked);
+  void checkItem(ShoppingItem item, bool checked) => _realm.write(() => item.checked = checked);
 
-  void deleteItem(ShoppingItem item) => appRealm.write(() => appRealm.delete(item));
+  void deleteItem(ShoppingItem item) => _realm.write(() => _realm.delete(item));
 
-  void deleteAllItems() => appRealm.write(() => appRealm.deleteAll<ShoppingItem>());
+  void deleteAllItems() => _realm.write(() => _realm.deleteAll<ShoppingItem>());
 
   void reorderItems(ShoppingServiceResult<ShoppingItem> existingItems, List<ShoppingItem> updatedItems) {
-    appRealm.write(() {
+    _realm.write(() {
       for (int i = 0; i < updatedItems.length; i++) {
         final item = existingItems.singleWhere((element) => element.id == updatedItems[i].id);
         if (item.order != i) {
